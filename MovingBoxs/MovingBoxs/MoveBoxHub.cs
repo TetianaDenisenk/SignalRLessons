f08fbee0c508fbebabfae872330ebd9dbe22999d
tetiana.kozlovska15 <tetiana.kozlovska15@gmail.com>
2017-12-02 12:30:35 +0200

    add Default.html, MoveBoxHub class

---
+++ MovingBoxs/MovingBoxs/MoveBoxHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovingBoxs
+{
+    public class MoveBoxHub : Hub
+    {
+
+        public void Move(int x,int y)
+        {
+            Clients.All.MoveIt(x, y);
+        }
+    }
+}
